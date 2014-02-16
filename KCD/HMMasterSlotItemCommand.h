56b8076ab205ca3a5e421ac6137b0aa9eead0ab0
masakih <masakih@users.sourceforge.jp>
2014-02-17 00:28:55 +0900

    /kcsapi/api_get_master/slotitemに対応

---
+++ KCD/HMMasterSlotItemCommand.h
@@ -0,0 +1,13 @@
+//
+//  HMMasterSlotItemCommand.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2014/02/16.
+//  Copyright (c) 2014年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMJSONCommand.h"
+
+@interface HMMasterSlotItemCommand : HMJSONCommand
+
+@end
---
+++ KCD/HMMasterSlotItemCommand.m
@@ -0,0 +1,81 @@
+//
+//  HMMasterSlotItemCommand.m
+//  KCD
+//
+//  Created by Hori,Masaki on 2014/02/16.
+//  Copyright (c) 2014年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMMasterSlotItemCommand.h"
+
+@implementation HMMasterSlotItemCommand
++ (void)load
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		[HMJSONCommand registerClass:self];
+	});
+}
+
++ (BOOL)canExcuteAPI:(NSString *)api
+{
+	return [api isEqualToString:@"/kcsapi/api_get_master/slotitem"];
+}
+- (void)execute
+{
+	NSArray *api_data = [self.json objectForKey:@"api_data"];
+	if(![api_data isKindOfClass:[NSArray class]]) {
+		[self log:@"api_data is NOT NSArray."];
+		return;
+	}
+	
+	NSManagedObjectContext *managedObjectContext = [[NSApp delegate] managedObjectContext];
+	
+	for(NSDictionary *type in api_data) {
+		NSString *stypeID = type[@"api_id"];
+		NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:@"MasterSlotItem"];
+		NSPredicate *predicate = [NSPredicate predicateWithFormat:@"id = %@", stypeID];
+		[req setPredicate:predicate];
+		NSError *error = nil;
+		id result = [managedObjectContext executeFetchRequest:req
+														error:&error];
+		if(error) {
+			[self log:@"Fetch error: %@", error];
+			continue;
+		}
+		NSManagedObject *object = nil;
+		if(!result || [result count] == 0) {
+			object = [NSEntityDescription insertNewObjectForEntityForName:@"MasterSlotItem"
+												   inManagedObjectContext:managedObjectContext];
+		} else {
+			object = result[0];
+		}
+		
+		for(NSString *key in type) {
+			id value = type[key];
+			if([value isKindOfClass:[NSArray class]]) {
+				NSUInteger i = 0;
+				for(id element in value) {
+					id hoge = element;
+					if([object validateValue:&hoge forKey:key error:NULL]) {
+						[object setValue:hoge forKey:[NSString stringWithFormat:@"%@%ld", key, i]];
+					}
+					i++;
+				}
+			} else if([value isKindOfClass:[NSDictionary class]]) {
+				for(id subKey in value) {
+					id subValue = value[subKey];
+					NSString *newKey = [NSString stringWithFormat:@"%@_D_%@", key, keyByDeletingPrefix(subKey)];
+					if([object validateValue:&subValue forKey:newKey error:NULL]) {
+						[object setValue:subValue forKey:newKey];
+					}
+				}
+			} else {
+				if([object validateValue:&value forKey:key error:NULL]) {
+					[object setValue:value forKey:key];
+				}
+			}
+		}
+	}
+}
+@end
