1b6d39810ba88537ef4cdd9972eb2adb0da1a4d7
masakih <masakih@users.sourceforge.jp>
2014-02-24 21:36:07 +0900

    取得後に破棄した艦娘のデータが残るのでそれを削除するようにした

--- KCD/HMMemberShipCommand.m
+++ KCD/HMMemberShipCommand.m
@@ -8,6 +8,10 @@
 
 #import "HMMemberShipCommand.h"
 
+@interface HMMemberShipCommand ()
+@property (strong) NSMutableArray *ids;
+@end
+
 @implementation HMMemberShipCommand
 + (void)load
 {
@@ -21,8 +25,45 @@
 {
 	return [api isEqualToString:@"/kcsapi/api_get_member/ship"];
 }
+
+- (id)init
+{
+	self = [super init];
+	if(self) {
+		_ids = [NSMutableArray new];
+	}
+	return self;
+}
 - (void)execute
 {
 	[self commitJSONToEntityNamed:@"Ship"];
 }
+
+// 取得後破棄した艦娘のデータを削除する
+- (BOOL)handleExtraValue:(id)value forKey:(NSString *)key toObject:(NSManagedObject *)object
+{
+	if([key isEqualToString:@"api_id"]) {
+		[self.ids addObject:value];
+	}
+	return NO;
+}
+
+- (void)finishOperating:(NSManagedObjectContext *)moc
+{
+	NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Ship"];
+	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"NOT id IN %@", self.ids];
+	[request setPredicate:predicate];
+	
+	NSError *error = nil;
+	NSArray *array = [moc executeFetchRequest:request error:&error];
+	if(error) {
+		NSLog(@"HOGEEEEE");
+		return;
+	}
+	
+	for(id obj in array) {
+		[moc deleteObject:obj];
+	}
+}
+
 @end
