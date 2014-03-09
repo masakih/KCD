b02048b298bb3b05d4b16cf3719a9a2c9556710d
masakih <masakih@users.sourceforge.jp>
2014-03-09 17:38:17 +0900

    入渠ステータス関連をクラスにわけた

--- KCD/HMDocksViewController.m
+++ KCD/HMDocksViewController.m
@@ -11,16 +11,10 @@
 #import "HMCoreDataManager.h"
 
 #import "HMMissionStatus.h"
+#import "HMNyukyoDockStatus.h"
 
 
 @interface HMDocksViewController ()
-
-
-// 入渠中フラグ
-@property BOOL nDock1Flag;
-@property BOOL nDock2Flag;
-@property BOOL nDock3Flag;
-@property BOOL nDock4Flag;
 
 
 // NSUserNotifyを行ったか
@@ -39,6 +33,12 @@
 @property (strong) HMMissionStatus *mission2Status;
 @property (strong) HMMissionStatus *mission3Status;
 @property (strong) HMMissionStatus *mission4Status;
+
+@property (strong) HMNyukyoDockStatus *ndock1Status;
+@property (strong) HMNyukyoDockStatus *ndock2Status;
+@property (strong) HMNyukyoDockStatus *ndock3Status;
+@property (strong) HMNyukyoDockStatus *ndock4Status;
+
 
 @end
 
@@ -87,6 +87,26 @@
 		
 		_mission4Status = [[HMMissionStatus alloc] initWithDeckNumber:4];
 		_mission4Status.managedObjectContext = self.managedObjectContext;
+		
+		_ndock1Status = [[HMNyukyoDockStatus alloc] initWithDockNumber:1];
+		_ndock1Status.managedObjectContext = self.managedObjectContext;
+		[self bind:@"nDock1Time" toObject:self.ndock1Status withKeyPath:@"time" options:nil];
+		[self bind:@"nDock1ShipName" toObject:self.ndock1Status withKeyPath:@"name" options:nil];
+		
+		_ndock2Status = [[HMNyukyoDockStatus alloc] initWithDockNumber:2];
+		_ndock2Status.managedObjectContext = self.managedObjectContext;
+		[self bind:@"nDock2Time" toObject:self.ndock2Status withKeyPath:@"time" options:nil];
+		[self bind:@"nDock2ShipName" toObject:self.ndock2Status withKeyPath:@"name" options:nil];
+		
+		_ndock3Status = [[HMNyukyoDockStatus alloc] initWithDockNumber:3];
+		_ndock3Status.managedObjectContext = self.managedObjectContext;
+		[self bind:@"nDock3Time" toObject:self.ndock3Status withKeyPath:@"time" options:nil];
+		[self bind:@"nDock3ShipName" toObject:self.ndock3Status withKeyPath:@"name" options:nil];
+		
+		_ndock4Status = [[HMNyukyoDockStatus alloc] initWithDockNumber:4];
+		_ndock4Status.managedObjectContext = self.managedObjectContext;
+		[self bind:@"nDock4Time" toObject:self.ndock4Status withKeyPath:@"time" options:nil];
+		[self bind:@"nDock4ShipName" toObject:self.ndock4Status withKeyPath:@"name" options:nil];
 	}
 	return self;
 }
@@ -125,58 +145,8 @@
 {
 	return self.mission4Status.time;
 }
-- (void)checkNameForKey:(NSString *)nameKey flagKey:(NSString *)flagKey timeValue:(id)timeValue
-{
-	BOOL flag = [[self valueForKey:flagKey] boolValue];
-	if(flag && !timeValue) {
-		[self setValue:@NO forKey:flagKey];
-		[self willChangeValueForKey:nameKey];
-		[self didChangeValueForKey:nameKey];
-	} else if(!flag && timeValue) {
-		[self setValue:@YES forKey:flagKey];
-		[self willChangeValueForKey:nameKey];
-		[self didChangeValueForKey:nameKey];
-	}
-}
 
 #pragma mark - Docking
-- (NSString *)shipNameForNDockNumber:(NSUInteger)number
-{
-	static NSArray *shipIdKeys = nil;
-	if(!shipIdKeys) {
-		shipIdKeys = @[@"nDock1.selection.ship_id", @"nDock2.selection.ship_id", @"nDock3.selection.ship_id", @"nDock4.selection.ship_id"];
-	}
-	
-	NSNumber *nDockShipId = [self valueForKeyPath:shipIdKeys[number - 1]];
-	if(![nDockShipId isKindOfClass:[NSNumber class]]) return nil;
-	if([nDockShipId integerValue] == 0) return nil;
-	
-	NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Ship"];
-	NSPredicate *predicate = [NSPredicate predicateWithFormat:@"id = %@", nDockShipId];
-	[request setPredicate:predicate];
-	
-	NSArray *array = [self.managedObjectContext executeFetchRequest:request error:NULL];
-	if([array count] == 0) return @"Unknown";
-	
-	NSString *name = [array[0] valueForKeyPath:@"master_ship.name"];
-	return name;
-}
-- (NSString *)nDock1ShipName
-{
-	return [self shipNameForNDockNumber:1];
-}
-- (NSString *)nDock2ShipName
-{
-	return [self shipNameForNDockNumber:2];
-}
-- (NSString *)nDock3ShipName
-{
-	return [self shipNameForNDockNumber:3];
-}
-- (NSString *)nDock4ShipName
-{
-	return [self shipNameForNDockNumber:4];
-}
 
 - (NSNumber *)nDockTimeForNDock:(NSObjectController *)nDock
 {
@@ -196,33 +166,6 @@
 		return @( - [[NSTimeZone systemTimeZone] secondsFromGMT]);
 	} else {
 		return @(diff - [[NSTimeZone systemTimeZone] secondsFromGMT]);
-	}
-}
-- (void)notifyIfNeededFinishDockingNumber:(NSUInteger)number
-{
-	static NSArray *timeKeys = nil;
-	if(!timeKeys) {
-		timeKeys = @[@"nDock1Time", @"nDock2Time", @"nDock3Time", @"nDock4Time"];
-	}
-	static NSArray *notifiedKeys = nil;
-	if(!notifiedKeys) {
-		notifiedKeys = @[@"nDock1Notified", @"nDock2Notified", @"nDock3Notified", @"nDock4Notified"];
-	}
-	
-	NSTimeInterval time = [[self valueForKey:timeKeys[number - 1]] doubleValue];
-	
-	if(time < 1 * 60 - [[NSTimeZone systemTimeZone] secondsFromGMT]) {
-		BOOL flag = [[self valueForKey:notifiedKeys[number -1]] boolValue];
-		if(!flag) {
-			NSUserNotification * notification = [NSUserNotification new];
-			NSString *format = NSLocalizedString(@"%@ Will Finish Docking.", @"%@ Will Finish Docking.");
-			notification.title = [NSString stringWithFormat:format, [self shipNameForNDockNumber:number]];
-			notification.informativeText = [NSString stringWithFormat:format, [self shipNameForNDockNumber:number]];
-			[[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
-			[self setValue:@YES forKey:notifiedKeys[number - 1]];
-		}
-	} else {
-		[self setValue:@NO forKey:notifiedKeys[number - 1]];
 	}
 }
 - (void)notifyIfNeededFinishBuildAtDockNumber:(NSUInteger)number
@@ -256,19 +199,10 @@
 - (void)fire:(id)timer
 {
 	// 入渠ドック
-	self.nDock1Time = [self nDockTimeForNDock:self.nDock1];
-	self.nDock2Time = [self nDockTimeForNDock:self.nDock2];
-	self.nDock3Time = [self nDockTimeForNDock:self.nDock3];
-	self.nDock4Time = [self nDockTimeForNDock:self.nDock4];
-	[self checkNameForKey:@"nDock1ShipName" flagKey:@"nDock1Flag" timeValue:self.nDock1Time];
-	[self checkNameForKey:@"nDock2ShipName" flagKey:@"nDock2Flag" timeValue:self.nDock2Time];
-	[self checkNameForKey:@"nDock3ShipName" flagKey:@"nDock3Flag" timeValue:self.nDock3Time];
-	[self checkNameForKey:@"nDock4ShipName" flagKey:@"nDock4Flag" timeValue:self.nDock4Time];
-	
-	[self notifyIfNeededFinishDockingNumber:1];
-	[self notifyIfNeededFinishDockingNumber:2];
-	[self notifyIfNeededFinishDockingNumber:3];
-	[self notifyIfNeededFinishDockingNumber:4];
+	[self.ndock1Status update];
+	[self.ndock2Status update];
+	[self.ndock3Status update];
+	[self.ndock4Status update];
 	
 	// 建造ドック
 	self.kDock1Time = [self nDockTimeForNDock:self.kDock1];
---
+++ KCD/HMNyukyoDockStatus.h
@@ -0,0 +1,23 @@
+//
+//  HMNyukyoDockStatus.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2014/03/09.
+//  Copyright (c) 2014年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMNyukyoDockStatus : NSObject
+
+- (id)initWithDockNumber:(NSInteger)dockNumber;
+
+@property (assign) NSManagedObjectContext *managedObjectContext;
+
+@property (readonly) NSString *name;
+@property (readonly) NSNumber *time;
+@property (readonly) BOOL isTasking;
+@property (readonly) BOOL didNotify;
+
+- (void)update;
+@end
