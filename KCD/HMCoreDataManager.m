0e62bb8322101d558ccbf8924149c8d1364a5443
masakih <masakih@users.sourceforge.jp>
2014-03-22 21:19:33 +0900

    コンテクストのマージに失敗する問題を解消

--- KCD/HMCoreDataManager.m
+++ KCD/HMCoreDataManager.m
@@ -170,12 +170,6 @@ static NSManagedObjectModel *_managedObjectModel = nil;
     return _managedObjectContext;
 }
 
-// Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
-- (NSUndoManager *)windowWillReturnUndoManager:(NSWindow *)window
-{
-    return [[self managedObjectContext] undoManager];
-}
-
 // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
 - (IBAction)saveAction:(id)sender
 {
@@ -190,52 +184,6 @@ static NSManagedObjectModel *_managedObjectModel = nil;
     }
 }
 
-- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender
-{
-    // Save changes in the application's managed object context before the application terminates.
-    
-    if (!_managedObjectContext) {
-        return NSTerminateNow;
-    }
-    
-    if (![[self managedObjectContext] commitEditing]) {
-        NSLog(@"%@:%@ unable to commit editing to terminate", [self class], NSStringFromSelector(_cmd));
-        return NSTerminateCancel;
-    }
-    
-    if (![[self managedObjectContext] hasChanges]) {
-        return NSTerminateNow;
-    }
-    
-    NSError *error = nil;
-    if (![[self managedObjectContext] save:&error]) {
-		
-        // Customize this code block to include application-specific recovery steps.
-        BOOL result = [sender presentError:error];
-        if (result) {
-            return NSTerminateCancel;
-        }
-		
-        NSString *question = NSLocalizedString(@"Could not save changes while quitting. Quit anyway?", @"Quit without saves error question message");
-        NSString *info = NSLocalizedString(@"Quitting now will lose any changes you have made since the last successful save", @"Quit without saves error question info");
-        NSString *quitButton = NSLocalizedString(@"Quit anyway", @"Quit anyway button title");
-        NSString *cancelButton = NSLocalizedString(@"Cancel", @"Cancel button title");
-        NSAlert *alert = [[NSAlert alloc] init];
-        [alert setMessageText:question];
-        [alert setInformativeText:info];
-        [alert addButtonWithTitle:quitButton];
-        [alert addButtonWithTitle:cancelButton];
-		
-        NSInteger answer = [alert runModal];
-        
-        if (answer == NSAlertAlternateReturn) {
-            return NSTerminateCancel;
-        }
-    }
-	
-    return NSTerminateNow;
-}
-
 - (void)applicationWillTerminate:(NSNotification *)notification
 {
 	[self saveAction:nil];
@@ -243,6 +191,10 @@ static NSManagedObjectModel *_managedObjectModel = nil;
 
 - (void)anotherContextDidSave:(NSNotification *)notification
 {
+	NSManagedObjectContext *moc = [notification object];
+	NSPersistentStoreCoordinator *psc = [moc persistentStoreCoordinator];
+	if(![psc isEqual:_persistentStoreCoordinator]) return;
+	
 	if([NSThread isMainThread]) {
 		[self.managedObjectContext mergeChangesFromContextDidSaveNotification:notification];
 	} else {
