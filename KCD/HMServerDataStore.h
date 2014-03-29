4f6dbd140551daf3374cb067e7aa326606c79af2
masakih <masakih@users.sourceforge.jp>
2014-03-29 23:13:16 +0900

    サーバーからもらうデータ用にサブクラス作成

---
+++ KCD/HMServerDataStore.h
@@ -0,0 +1,13 @@
+//
+//  HMServerDataStore.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2014/03/29.
+//  Copyright (c) 2014年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMCoreDataManager.h"
+
+@interface HMServerDataStore : HMCoreDataManager
+
+@end
