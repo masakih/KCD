6c1e4451f4ec74996c3014d40849c66c223b860b
masakih <masakih@users.sourceforge.jp>
2014-05-01 00:01:19 +0900

    装備一覧で種別を表示できるようにした

---
+++ KCD/HMSlotItemEquipTypeTransformer.h
@@ -0,0 +1,13 @@
+//
+//  HMSlotItemEquipTypeTransformer.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2014/04/30.
+//  Copyright (c) 2014年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMSlotItemEquipTypeTransformer : NSValueTransformer
+
+@end
