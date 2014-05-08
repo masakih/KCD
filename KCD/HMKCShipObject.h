0574559c477fa64541e20dbb4123a395dc6df025
masakih <masakih@users.sourceforge.jp>
2014-05-08 22:04:43 +0900

    shortTypeNameの関連キーパスを設定 プロパティーをすべて列挙＆不要な更新を行わないようにした

--- KCD/HMKCShipObject.h
+++ KCD/HMKCShipObject.h
@@ -12,4 +12,56 @@
 
 @property (readonly) NSColor *statusColor;
 @property (readonly) NSColor *conditionColor;
+
+@property (readonly) NSString *shortTypeName;
+
+
+@property (nonatomic, retain) NSNumber * bull;
+@property (nonatomic, retain) NSNumber * cond;
+@property (nonatomic, retain) NSNumber * exp;
+@property (nonatomic, retain) NSNumber * fuel;
+@property (nonatomic, retain) NSNumber * id;
+@property (nonatomic, retain) NSNumber * kaihi_0;
+@property (nonatomic, retain) NSNumber * kaihi_1;
+@property (nonatomic, retain) NSNumber * karyoku_0;
+@property (nonatomic, retain) NSNumber * karyoku_1;
+@property (nonatomic, retain) NSNumber * kyouka_0;
+@property (nonatomic, retain) NSNumber * kyouka_1;
+@property (nonatomic, retain) NSNumber * kyouka_2;
+@property (nonatomic, retain) NSNumber * kyouka_3;
+@property (nonatomic, retain) NSNumber * kyouka_4;
+@property (nonatomic, retain) NSNumber * locked;
+@property (nonatomic, retain) NSNumber * lucky_0;
+@property (nonatomic, retain) NSNumber * lucky_1;
+@property (nonatomic, retain) NSNumber * lv;
+@property (nonatomic, retain) NSNumber * maxhp;
+@property (nonatomic, retain) NSNumber * ndock_time;
+@property (nonatomic, retain) NSNumber * nowhp;
+@property (nonatomic, retain) NSNumber * onslot_0;
+@property (nonatomic, retain) NSNumber * onslot_1;
+@property (nonatomic, retain) NSNumber * onslot_2;
+@property (nonatomic, retain) NSNumber * onslot_3;
+@property (nonatomic, retain) NSNumber * onslot_4;
+@property (nonatomic, retain) NSNumber * raisou_0;
+@property (nonatomic, retain) NSNumber * raisou_1;
+@property (nonatomic, retain) NSNumber * sakuteki_0;
+@property (nonatomic, retain) NSNumber * sakuteki_1;
+@property (nonatomic, retain) NSNumber * ship_id;
+@property (nonatomic, retain) NSNumber * slot_0;
+@property (nonatomic, retain) NSNumber * slot_1;
+@property (nonatomic, retain) NSNumber * slot_2;
+@property (nonatomic, retain) NSNumber * slot_3;
+@property (nonatomic, retain) NSNumber * slot_4;
+@property (nonatomic, retain) NSNumber * sortno;
+@property (nonatomic, retain) NSNumber * soukou_0;
+@property (nonatomic, retain) NSNumber * soukou_1;
+@property (nonatomic, retain) NSNumber * srate;
+@property (nonatomic, retain) NSNumber * taiku_0;
+@property (nonatomic, retain) NSNumber * taiku_1;
+@property (nonatomic, retain) NSNumber * taisen_0;
+@property (nonatomic, retain) NSNumber * taisen_1;
+@property (nonatomic, retain) NSNumber * use_bull;
+@property (nonatomic, retain) NSNumber * use_fuel;
+@property (nonatomic, retain) HMKCManagedObject *master_ship;
+
 @end
--- KCD/HMKCShipObject.m
+++ KCD/HMKCShipObject.m
@@ -11,7 +11,157 @@
 static NSArray *shortSTypeNames = nil;
 static NSArray *levelUpExps = nil;
 
+@interface HMKCManagedObject ()
+
+@property (nonatomic, retain) NSNumber * primitiveBull;
+@property (nonatomic, retain) NSNumber * primitiveCond;
+@property (nonatomic, retain) NSNumber * primitiveExp;
+@property (nonatomic, retain) NSNumber * primitiveFuel;
+@property (nonatomic, retain) NSNumber * primitiveId;
+@property (nonatomic, retain) NSNumber * primitiveKaihi_0;
+@property (nonatomic, retain) NSNumber * primitiveKaihi_1;
+@property (nonatomic, retain) NSNumber * primitiveKaryoku_0;
+@property (nonatomic, retain) NSNumber * primitiveKaryoku_1;
+@property (nonatomic, retain) NSNumber * primitiveKyouka_0;
+@property (nonatomic, retain) NSNumber * primitiveKyouka_1;
+@property (nonatomic, retain) NSNumber * primitiveKyouka_2;
+@property (nonatomic, retain) NSNumber * primitiveKyouka_3;
+@property (nonatomic, retain) NSNumber * primitiveKyouka_4;
+@property (nonatomic, retain) NSNumber * primitiveLocked;
+@property (nonatomic, retain) NSNumber * primitiveLucky_0;
+@property (nonatomic, retain) NSNumber * primitiveLucky_1;
+@property (nonatomic, retain) NSNumber * primitiveLv;
+@property (nonatomic, retain) NSNumber * primitiveMaxhp;
+@property (nonatomic, retain) NSNumber * primitiveNdock_time;
+@property (nonatomic, retain) NSNumber * primitiveNowhp;
+@property (nonatomic, retain) NSNumber * primitiveOnslot_0;
+@property (nonatomic, retain) NSNumber * primitiveOnslot_1;
+@property (nonatomic, retain) NSNumber * primitiveOnslot_2;
+@property (nonatomic, retain) NSNumber * primitiveOnslot_3;
+@property (nonatomic, retain) NSNumber * primitiveOnslot_4;
+@property (nonatomic, retain) NSNumber * primitiveRaisou_0;
+@property (nonatomic, retain) NSNumber * primitiveRaisou_1;
+@property (nonatomic, retain) NSNumber * primitiveSakuteki_0;
+@property (nonatomic, retain) NSNumber * primitiveSakuteki_1;
+@property (nonatomic, retain) NSNumber * primitiveShip_id;
+@property (nonatomic, retain) NSNumber * primitiveSlot_0;
+@property (nonatomic, retain) NSNumber * primitiveSlot_1;
+@property (nonatomic, retain) NSNumber * primitiveSlot_2;
+@property (nonatomic, retain) NSNumber * primitiveSlot_3;
+@property (nonatomic, retain) NSNumber * primitiveSlot_4;
+@property (nonatomic, retain) NSNumber * primitiveSortno;
+@property (nonatomic, retain) NSNumber * primitiveSoukou_0;
+@property (nonatomic, retain) NSNumber * primitiveSoukou_1;
+@property (nonatomic, retain) NSNumber * primitiveSrate;
+@property (nonatomic, retain) NSNumber * primitiveTaiku_0;
+@property (nonatomic, retain) NSNumber * primitiveTaiku_1;
+@property (nonatomic, retain) NSNumber * primitiveTaisen_0;
+@property (nonatomic, retain) NSNumber * primitiveTaisen_1;
+@property (nonatomic, retain) NSNumber * primitiveUse_bull;
+@property (nonatomic, retain) NSNumber * primitiveUse_fuel;
+
+@property (nonatomic, strong) NSManagedObject *primitiveMaster_ship;
+
+
+@end
+
 @implementation HMKCShipObject
+@dynamic bull;
+@dynamic cond;
+@dynamic exp;
+@dynamic fuel;
+@dynamic id;
+@dynamic kaihi_0;
+@dynamic kaihi_1;
+@dynamic karyoku_0;
+@dynamic karyoku_1;
+@dynamic kyouka_0;
+@dynamic kyouka_1;
+@dynamic kyouka_2;
+@dynamic kyouka_3;
+@dynamic kyouka_4;
+@dynamic locked;
+@dynamic lucky_0;
+@dynamic lucky_1;
+@dynamic lv;
+@dynamic maxhp;
+@dynamic ndock_time;
+@dynamic nowhp;
+@dynamic onslot_0;
+@dynamic onslot_1;
+@dynamic onslot_2;
+@dynamic onslot_3;
+@dynamic onslot_4;
+@dynamic raisou_0;
+@dynamic raisou_1;
+@dynamic sakuteki_0;
+@dynamic sakuteki_1;
+@dynamic ship_id;
+@dynamic slot_0;
+@dynamic slot_1;
+@dynamic slot_2;
+@dynamic slot_3;
+@dynamic slot_4;
+@dynamic sortno;
+@dynamic soukou_0;
+@dynamic soukou_1;
+@dynamic srate;
+@dynamic taiku_0;
+@dynamic taiku_1;
+@dynamic taisen_0;
+@dynamic taisen_1;
+@dynamic use_bull;
+@dynamic use_fuel;
+
+@dynamic primitiveBull;
+@dynamic primitiveCond;
+@dynamic primitiveExp;
+@dynamic primitiveFuel;
+@dynamic primitiveId;
+@dynamic primitiveKaihi_0;
+@dynamic primitiveKaihi_1;
+@dynamic primitiveKaryoku_0;
+@dynamic primitiveKaryoku_1;
+@dynamic primitiveKyouka_0;
+@dynamic primitiveKyouka_1;
+@dynamic primitiveKyouka_2;
+@dynamic primitiveKyouka_3;
+@dynamic primitiveKyouka_4;
+@dynamic primitiveLocked;
+@dynamic primitiveLucky_0;
+@dynamic primitiveLucky_1;
+@dynamic primitiveLv;
+@dynamic primitiveMaxhp;
+@dynamic primitiveNdock_time;
+@dynamic primitiveNowhp;
+@dynamic primitiveOnslot_0;
+@dynamic primitiveOnslot_1;
+@dynamic primitiveOnslot_2;
+@dynamic primitiveOnslot_3;
+@dynamic primitiveOnslot_4;
+@dynamic primitiveRaisou_0;
+@dynamic primitiveRaisou_1;
+@dynamic primitiveSakuteki_0;
+@dynamic primitiveSakuteki_1;
+@dynamic primitiveShip_id;
+@dynamic primitiveSlot_0;
+@dynamic primitiveSlot_1;
+@dynamic primitiveSlot_2;
+@dynamic primitiveSlot_3;
+@dynamic primitiveSlot_4;
+@dynamic primitiveSortno;
+@dynamic primitiveSoukou_0;
+@dynamic primitiveSoukou_1;
+@dynamic primitiveSrate;
+@dynamic primitiveTaiku_0;
+@dynamic primitiveTaiku_1;
+@dynamic primitiveTaisen_0;
+@dynamic primitiveTaisen_1;
+@dynamic primitiveUse_bull;
+@dynamic primitiveUse_fuel;
+
+@dynamic primitiveMaster_ship;
+
 + (void)load
 {
 	static dispatch_once_t onceToken;
@@ -40,6 +190,269 @@ static NSArray *levelUpExps = nil;
 + (NSSet *)keyPathsForValuesAffectingConditionColor
 {
 	return [NSSet setWithObjects:@"cond", nil];
+}
++ (NSSet *)keyPathsForValuesAffectingShortTypeName
+{
+	return [NSSet setWithObjects:@"master_ship.stype.id", @"master_ship.stype", @"master_ship", nil];
+}
+
+- (void)setBull:(NSNumber *)bull
+{
+	if([self.primitiveBull isEqual:bull]) return;
+	
+	self.primitiveBull = bull;
+}
+- (void)setCond:(NSNumber *)cond
+{
+	if([self.primitiveCond isEqual:cond]) return;
+	
+	self.primitiveCond = cond;
+}
+- (void)setExp:(NSNumber *)exp
+{
+	if([self.primitiveExp isEqual:exp]) return;
+	
+	self.primitiveExp = exp;
+}
+- (void)setFuel:(NSNumber *)fuel
+{
+	if([self.primitiveFuel isEqual:fuel]) return;
+	
+	self.primitiveFuel = fuel;
+}
+- (void)setId:(NSNumber *)id
+{
+	if([self.primitiveId isEqual:id]) return;
+	
+	self.primitiveId = id;
+}
+- (void)setKaihi_0:(NSNumber *)kaihi_0
+{
+	if([self.primitiveKaihi_0 isEqual:kaihi_0]) return;
+	
+	self.primitiveKaihi_0 = kaihi_0;
+}
+- (void)setKaihi_1:(NSNumber *)kaihi_1
+{
+	if([self.primitiveKaihi_1 isEqual:kaihi_1]) return;
+	
+	self.primitiveKaihi_1 = kaihi_1;
+}
+- (void)setKaryoku_0:(NSNumber *)karyoku_0
+{
+	if([self.primitiveKaryoku_0 isEqual:karyoku_0]) return;
+	
+	self.primitiveKaryoku_0 = karyoku_0;
+}
+- (void)setKaryoku_1:(NSNumber *)karyoku_1
+{
+	if([self.primitiveKaryoku_1 isEqual:karyoku_1]) return;
+	
+	self.primitiveKaryoku_1 = karyoku_1;
+}
+- (void)setLocked:(NSNumber *)locked
+{
+	if([self.primitiveLocked isEqual:locked]) return;
+	
+	self.primitiveLocked = locked;
+}
+- (void)setLucky_0:(NSNumber *)lucky_0
+{
+	if([self.primitiveLucky_0 isEqual:lucky_0]) return;
+	
+	self.primitiveLucky_0 = lucky_0;
+}
+- (void)setLucky_1:(NSNumber *)lucky_1
+{
+	if([self.primitiveLucky_1 isEqual:lucky_1]) return;
+	
+	self.primitiveLucky_1 = lucky_1;
+}
+- (void)setLv:(NSNumber *)lv
+{
+	if([self.primitiveLv isEqual:lv]) return;
+	
+	self.primitiveLv = lv;
+}
+- (void)setMaxhp:(NSNumber *)maxhp
+{
+	if([self.primitiveMaxhp isEqual:maxhp]) return;
+	
+	self.primitiveMaxhp = maxhp;
+}
+- (void)setNdock_time:(NSNumber *)ndock_time
+{
+	if([self.primitiveNdock_time isEqual:ndock_time]) return;
+	
+	self.primitiveNdock_time = ndock_time;
+}
+- (void)setNowhp:(NSNumber *)nowhp
+{
+	if([self.primitiveNowhp isEqual:nowhp]) return;
+	
+	self.primitiveNowhp = nowhp;
+}
+- (void)setOnslot_0:(NSNumber *)onslot_0
+{
+	if([self.primitiveOnslot_0 isEqual:onslot_0]) return;
+	
+	self.primitiveOnslot_0 = onslot_0;
+}
+- (void)setOnslot_1:(NSNumber *)onslot_1
+{
+	if([self.primitiveOnslot_1 isEqual:onslot_1]) return;
+	
+	self.primitiveOnslot_1 = onslot_1;
+}
+- (void)setOnslot_2:(NSNumber *)onslot_2
+{
+	if([self.primitiveOnslot_2 isEqual:onslot_2]) return;
+	
+	self.primitiveOnslot_2 = onslot_2;
+}
+- (void)setOnslot_3:(NSNumber *)onslot_3
+{
+	if([self.primitiveOnslot_3 isEqual:onslot_3]) return;
+	
+	self.primitiveOnslot_3 = onslot_3;
+}
+- (void)setOnslot_4:(NSNumber *)onslot_4
+{
+	if([self.primitiveOnslot_4 isEqual:onslot_4]) return;
+	
+	self.primitiveOnslot_4 = onslot_4;
+}
+- (void)setRaisou_0:(NSNumber *)raisou_0
+{
+	if([self.primitiveRaisou_0 isEqual:raisou_0]) return;
+	
+	self.primitiveRaisou_0 = raisou_0;
+}
+- (void)setRaisou_1:(NSNumber *)raisou_1
+{
+	if([self.primitiveRaisou_1 isEqual:raisou_1]) return;
+	
+	self.primitiveRaisou_1 = raisou_1;
+}
+- (void)setSakuteki_0:(NSNumber *)sakuteki_0
+{
+	if([self.primitiveSakuteki_0 isEqual:sakuteki_0]) return;
+	
+	self.primitiveSakuteki_0 = sakuteki_0;
+}
+- (void)setSakuteki_1:(NSNumber *)sakuteki_1
+{
+	if([self.primitiveSakuteki_1 isEqual:sakuteki_1]) return;
+	
+	self.primitiveSakuteki_1 = sakuteki_1;
+}
+- (void)setShip_id:(NSNumber *)ship_id
+{
+	if([self.primitiveShip_id isEqual:ship_id]) return;
+	
+	self.primitiveShip_id = ship_id;
+}
+- (void)setSlot_0:(NSNumber *)slot_0
+{
+	if([self.primitiveSlot_0 isEqual:slot_0]) return;
+	
+	self.primitiveSlot_0 = slot_0;
+}
+- (void)setSlot_1:(NSNumber *)slot_1
+{
+	if([self.primitiveSlot_1 isEqual:slot_1]) return;
+	
+	self.primitiveSlot_1 = slot_1;
+}
+- (void)setSlot_2:(NSNumber *)slot_2
+{
+	if([self.primitiveSlot_2 isEqual:slot_2]) return;
+	
+	self.primitiveSlot_2 = slot_2;
+}
+- (void)setSlot_3:(NSNumber *)slot_3
+{
+	if([self.primitiveSlot_3 isEqual:slot_3]) return;
+	
+	self.primitiveSlot_3 = slot_3;
+}
+- (void)setSlot_4:(NSNumber *)slot_4
+{
+	if([self.primitiveSlot_4 isEqual:slot_4]) return;
+	
+	self.primitiveSlot_4 = slot_4;
+}
+- (void)setSortno:(NSNumber *)sortno
+{
+	if([self.primitiveSortno isEqual:sortno]) return;
+	
+	self.primitiveSortno = sortno;
+}
+- (void)setSoukou_0:(NSNumber *)soukou_0
+{
+	if([self.primitiveSoukou_0 isEqual:soukou_0]) return;
+	
+	self.primitiveSoukou_0 = soukou_0;
+}
+- (void)setSoukou_1:(NSNumber *)soukou_1
+{
+	if([self.primitiveSoukou_1 isEqual:soukou_1]) return;
+	
+	self.primitiveSoukou_1 = soukou_1;
+}
+- (void)setSrate:(NSNumber *)srate
+{
+	if([self.primitiveSrate isEqual:srate]) return;
+	
+	self.primitiveSrate = srate;
+}
+- (void)setTaiku_0:(NSNumber *)taiku_0
+{
+	if([self.primitiveTaiku_0 isEqual:taiku_0]) return;
+	
+	self.primitiveTaiku_0 = taiku_0;
+}
+- (void)setTaiku_1:(NSNumber *)taiku_1
+{
+	if([self.primitiveTaiku_1 isEqual:taiku_1]) return;
+	
+	self.primitiveTaiku_1 = taiku_1;
+}
+- (void)setTaisen_0:(NSNumber *)taisen_0
+{
+	if([self.primitiveTaisen_0 isEqual:taisen_0]) return;
+	
+	self.primitiveTaisen_0 = taisen_0;
+}
+- (void)setTaisen_1:(NSNumber *)taisen_1
+{
+	if([self.primitiveTaisen_1 isEqual:taisen_1]) return;
+	
+	self.primitiveTaisen_1 = taisen_1;
+}
+- (void)setUse_bull:(NSNumber *)use_bull
+{
+	if([self.primitiveUse_bull isEqual:use_bull]) return;
+	
+	self.primitiveUse_bull = use_bull;
+}
+- (void)setUse_fuel:(NSNumber *)use_fuel
+{
+	if([self.primitiveUse_fuel isEqual:use_fuel]) return;
+	
+	self.primitiveUse_fuel = use_fuel;
+}
+
+- (void)setMaster_ship:(id)value
+{
+	if([self.primitiveMaster_ship isEqual:value]) return;
+	
+	NSLog(@"Ship did change master_ship.");
+	self.primitiveMaster_ship = value;
+}
+- (NSManagedObject *)master_ship
+{
+	return self.primitiveMaster_ship;
 }
 
 - (NSNumber *)master_sortno
