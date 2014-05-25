eef140c9e299f20b1dfc3b4c34387ccb4e8fbebb
masakih <masakih@users.sourceforge.jp>
2014-05-25 20:56:40 +0900

    大破チェック

--- KCD/HMCalculateDamageCommand.m
+++ KCD/HMCalculateDamageCommand.m
@@ -13,7 +13,7 @@
 
 @implementation HMCalculateDamageCommand
 
-- (void)resetDamage
+- (void)resetBattle
 {
 	HMTemporaryDataStore *store = [HMTemporaryDataStore oneTimeEditor];
 	NSManagedObjectContext *moc = store.managedObjectContext;
@@ -25,7 +25,15 @@
 		[moc deleteObject:object];
 	}
 	
-	array = [store objectsWithEntityName:@"Damage"
+	[store saveAction:nil];
+}
+
+- (void)resetDamage
+{
+	HMTemporaryDataStore *store = [HMTemporaryDataStore oneTimeEditor];
+	NSManagedObjectContext *moc = store.managedObjectContext;
+	
+	NSArray *array = [store objectsWithEntityName:@"Damage"
 							   predicate:nil
 								   error:NULL];
 	for(id object in array) {
@@ -79,30 +87,40 @@
 	}
 	
 	// koukuu
+	printf("Kouku damages-> ");
 	do {
 		id koukuDamage = [self.json valueForKeyPath:@"api_data.api_kouku.api_stage3.api_fdam"];
 		if(!koukuDamage || [koukuDamage isEqual:[NSNull null]]) break;
 		for(NSInteger i = 1; i <= 6; i++) {
 			NSInteger damage = [[koukuDamage objectAtIndex:i] integerValue];
-			id damageObject = [damages objectAtIndex:i];
+			
+			printf("%zd,%zd    ", i - 1, damage);
+			
+			id damageObject = [damages objectAtIndex:i - 1];
 			[damageObject setValue:@(damage) forKeyPath:@"damage"];
 		}
 	} while(NO);
+	printf("\n");
 	
 	// opening attack
+	printf("Opening attack damages-> ");
 	do {
 		id openigDamage = [self.json valueForKeyPath:@"api_data.api_opening_atack.api_fdam"];
 		if(!openigDamage || [openigDamage isEqual:[NSNull null]]) break;
-		for(NSInteger i = 0; i < 6; i++) {
-			NSInteger damage = [[openigDamage objectAtIndex:i] integerValue];
-			id damageObject = [damages objectAtIndex:i];
+		for(NSInteger i = 1; i <= 6; i++) {
+			NSInteger damage = [[openigDamage objectAtIndex:i - 1] integerValue];
+			
+			printf("%zd,%zd    ", i, damage);
+			
+			id damageObject = [damages objectAtIndex:i - 1];
 			damage += [[damageObject valueForKey:@"damage"] integerValue];
 			[damageObject setValue:@(damage) forKeyPath:@"damage"];
 		}
 	} while(NO);
-	
+	printf("\n");
 	
 	// hougeki1
+	printf("Hougeki1 damages-> ");
 	{
 		id targetShips = [self.json valueForKeyPath:@"api_data.api_hougeki1.api_df_list"];
 		id hougeki1Damages = [self.json valueForKeyPath:@"api_data.api_hougeki1.api_damage"];
@@ -121,8 +139,11 @@
 					continue;
 				}
 				
-				id damageObject = [damages objectAtIndex:target];
+				id damageObject = [damages objectAtIndex:target - 1];
 				NSInteger damage = [[[hougeki1Damages objectAtIndex:i] objectAtIndex:j] integerValue];
+				
+				printf("%zd,%zd    ", target - 1, damage);
+				
 				damage += [[damageObject valueForKey:@"damage"] integerValue];
 				[damageObject setValue:@(damage) forKeyPath:@"damage"];
 				
@@ -131,8 +152,10 @@
 			i++;
 		}
 	}
-
+	printf("\n");
+	
 	// hougeki2
+	printf("Hougeki2 damages-> ");
 	do {
 		id hasHougeki2 = [self.json valueForKeyPath:@"api_data.api_hougeki2"];
 		if(!hasHougeki2 || [hasHougeki2 isEqual:[NSNull null]]) break;
@@ -154,8 +177,11 @@
 					continue;
 				}
 				
-				id damageObject = [damages objectAtIndex:target];
+				id damageObject = [damages objectAtIndex:target - 1];
 				NSInteger damage = [[[hougeki1Damages objectAtIndex:i] objectAtIndex:j] integerValue];
+				
+				printf("%zd,%zd    ", target - 1, damage);
+				
 				damage += [[damageObject valueForKey:@"damage"] integerValue];
 				[damageObject setValue:@(damage) forKeyPath:@"damage"];
 				
@@ -164,19 +190,24 @@
 			i++;
 		}
 	} while(NO);
-	
+	printf("\n");
 	
 	// raigeki
+	printf("Raigeki damages-> ");
 	do {
 		id raigekiDamage = [self.json valueForKeyPath:@"api_data.api_raigeki.api_fdam"];
 		if(!raigekiDamage || [raigekiDamage isEqual:[NSNull null]]) break;
 		for(NSInteger i = 1; i <= 6; i++) {
 			NSInteger damage = [[raigekiDamage objectAtIndex:i] integerValue];
+			
+			printf("%zd,%zd    ", i - 1, damage);
+			
 			id damageObject = [damages objectAtIndex:i - 1];
 			damage += [[damageObject valueForKey:@"damage"] integerValue];
 			[damageObject setValue:@(damage) forKeyPath:@"damage"];
 		}
 	} while(NO);
+	printf("\n");
 	
 	[store saveAction:nil];
 }
@@ -199,6 +230,7 @@
 	}
 	
 	// hougeki1
+	printf("Yasen damages-> ");
 	{
 		id targetShips = [self.json valueForKeyPath:@"api_data.api_hougeki1.api_df_list"];
 		id hougeki1Damages = [self.json valueForKeyPath:@"api_data.api_hougeki1.api_damage"];
@@ -212,8 +244,11 @@
 					continue;
 				}
 				
-				id damageObject = [damages objectAtIndex:target];
+				id damageObject = [damages objectAtIndex:target - 1];
 				NSInteger damage = [[[hougeki1Damages objectAtIndex:i] objectAtIndex:j] integerValue];
+				
+				printf("%zd,%zd    ", target - 1, damage);
+				
 				damage += [[damageObject valueForKey:@"damage"] integerValue];
 				[damageObject setValue:@(damage) forKeyPath:@"damage"];
 				
@@ -222,10 +257,15 @@
 			i++;
 		}
 	}
+	printf("\n");
 	
 	[store saveAction:nil];
 }
 
+- (NSArray *)deckWithNumber:(NSNumber *)number
+{
+	return [NSArray array];
+}
 - (void)applyDamage
 {
 	HMTemporaryDataStore *store = [HMTemporaryDataStore oneTimeEditor];
@@ -235,7 +275,7 @@
 	NSArray *damages = nil;
 	
 	NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Damage"];
-	NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"id" ascending:NO];
+	NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"id" ascending:YES];
 	[request setSortDescriptors:@[sortDescriptor]];
 	damages = [moc executeFetchRequest:request error:NULL];
 	if(damages.count != 6) {
@@ -243,10 +283,50 @@
 		return;
 	}
 	
+	NSArray *array = [store objectsWithEntityName:@"Battle"
+										predicate:nil
+											error:NULL];
+	if(array.count == 0) {
+		NSLog(@"Battle is invalid. %s", __PRETTY_FUNCTION__);
+		return;
+	}
+	
 	HMServerDataStore *serverStore = [HMServerDataStore oneTimeEditor];
 	
-	// TODO:
-	NSLog(@"MUST IMPLEMENT!!");
+	NSArray *decks = [serverStore objectsWithEntityName:@"Deck"
+												  error:NULL
+										predicateFormat:@"id = %@", [array[0] valueForKey:@"deckId"]];
+	if(decks.count == 0) {
+		NSLog(@"Deck is invalid. %s", __PRETTY_FUNCTION__);
+		return;
+	}
+	id deck = decks[0];
+	NSMutableArray *shipIds = [NSMutableArray new];
+	[shipIds addObject:[deck valueForKey:@"ship_0"]];
+	[shipIds addObject:[deck valueForKey:@"ship_1"]];
+	[shipIds addObject:[deck valueForKey:@"ship_2"]];
+	[shipIds addObject:[deck valueForKey:@"ship_3"]];
+	[shipIds addObject:[deck valueForKey:@"ship_4"]];
+	[shipIds addObject:[deck valueForKey:@"ship_5"]];
+	
+	NSMutableArray *ships = [NSMutableArray new];
+	for(id shipId in shipIds) {
+		NSArray *ship = [serverStore objectsWithEntityName:@"Ship"
+													 error:NULL
+										   predicateFormat:@"id = %@", @([shipId integerValue])];
+		if(ship.count != 0 && ![ship[0] isEqual:[NSNull null]]) {
+			[ships addObject:ship[0]];
+		}
+	}
+	
+	NSUInteger shipCount = ships.count;
+	for(NSInteger i = 0; i < shipCount; i++) {
+		id ship = ships[i];
+		NSInteger damage = [[damages[i] valueForKey:@"damage"] integerValue];
+		NSInteger nowhp = [[ship valueForKey:@"nowhp"] integerValue];
+		nowhp -= damage;
+		[ship setValue:@(nowhp) forKeyPath:@"nowhp"];
+	}
 	
 	[serverStore saveAction:nil];
 }
@@ -254,7 +334,7 @@
 - (void)execute
 {
 	if([self.api isEqualToString:@"/kcsapi/api_req_map/start"]) {
-		[self resetDamage];
+		[self resetBattle];
 		[self startBattle];
 		return;
 	}
