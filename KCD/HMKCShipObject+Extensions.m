a1e9eff611ed8483e3b66b9e38782eb4c8c68933
masakih <masakih@users.sourceforge.jp>
2014-08-10 23:49:43 +0900

    AL/MI作戦の色を入れ替えた
    AL作戦参加艦娘はブルー、MI作戦参加艦娘はオレンジに変更

--- KCD/HMKCShipObject+Extensions.m
+++ KCD/HMKCShipObject+Extensions.m
@@ -539,10 +539,8 @@ static NSMutableDictionary *names = nil;
 - (NSColor *)planColor
 {
 	NSInteger planType = [[self valueForKey:@"sally_area"] integerValue];
-	if(planType == 1) return [NSColor colorWithCalibratedRed:0.800 green:0.223 blue:0.000 alpha:1.000];
-	
-	if(planType == 2) return [NSColor colorWithCalibratedRed:0.093 green:0.000 blue:0.800 alpha:1.000];
-	
+	if(planType == 1) return [NSColor colorWithCalibratedRed:0.000 green:0.043 blue:0.518 alpha:1.000];
+	if(planType == 2) return [NSColor colorWithCalibratedRed:0.800 green:0.223 blue:0.000 alpha:1.000];
 	return [NSColor controlTextColor];
 }
 @end
