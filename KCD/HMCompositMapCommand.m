3ba679c1cd844efee47c337234bcc3d31c56cf19
masakih <masakih@users.sourceforge.jp>
2015-01-11 22:46:15 +0900

    HMMasterSTypeCommandクラスをSwiftで書き換え

--- KCD/HMCompositMapCommand.m
+++ KCD/HMCompositMapCommand.m
@@ -8,7 +8,6 @@
 
 #import "HMCompositMapCommand.h"
 
-#import "HMMasterSTypeCommand.h"
 #import "HMMaserShipCommand.h"
 #import "HMMasterMissionCommand.h"
 #import "HMMasterSlotItemEquipTypeCommand.h"
