fb6be1167fd1b2178daf2cdfb6c4a10e99d4e792
masakih <masakih@users.sourceforge.jp>
2015-02-19 22:12:14 +0900

    ドロップ履歴を残すためのエンティティーを追加

---
+++ KCD/HMDropShipHistory.m
@@ -0,0 +1,23 @@
+//
+//  HMDropShipHistory.m
+//  KCD
+//
+//  Created by Hori,Masaki on 2015/02/18.
+//  Copyright (c) 2015年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMDropShipHistory.h"
+
+
+@implementation HMDropShipHistory
+
+@dynamic shipName;
+@dynamic mapArea;
+@dynamic mapInfo;
+@dynamic mapCell;
+@dynamic mapAreaName;
+@dynamic mapInfoName;
+@dynamic date;
+@dynamic winRank;
+
+@end
