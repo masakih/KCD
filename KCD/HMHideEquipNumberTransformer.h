112e618a7346997bf94163522a4e240f1c0a3339
masakih <masakih@users.sourceforge.jp>
2015-03-01 13:42:28 +0900

    艦載機でないとき艦載機数を表示しないようにした

---
+++ KCD/HMHideEquipNumberTransformer.h
@@ -0,0 +1,13 @@
+//
+//  HMHideEquipNumberTransformer.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2015/03/01.
+//  Copyright (c) 2015年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMHideEquipNumberTransformer : NSValueTransformer
+
+@end
---
+++ KCD/HMHideEquipNumberTransformer.m
@@ -0,0 +1,58 @@
+//
+//  HMHideEquipNumberTransformer.m
+//  KCD
+//
+//  Created by Hori,Masaki on 2015/03/01.
+//  Copyright (c) 2015年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMHideEquipNumberTransformer.h"
+#import "HMServerDataStore.h"
+
+#import "HMKCSlotItemObject+Extensions.h"
+#import "HMKCMasterSlotItemObject.h"
+
+static NSArray *showsTypes = nil;
+
+@implementation HMHideEquipNumberTransformer
++ (void)load
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		[NSValueTransformer setValueTransformer:[self new] forName:@"HMHideEquipNumberTransformer"];
+		
+		showsTypes = @[@6, @7, @8, @9, @10, @11, @25, @26];
+	});
+}
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
++ (BOOL)allowsReverseTransformation
+{
+	return NO;
+}
+
+- (id)transformedValue:(id)value
+{
+	if(![value isKindOfClass:[NSNumber class]]) return nil;
+	
+	NSInteger slotItemID = [value integerValue];
+	if(slotItemID == -1) return @YES;
+	
+	HMServerDataStore *store = [HMServerDataStore oneTimeEditor];
+	
+	NSError *error = nil;
+	NSArray *array = [store objectsWithEntityName:@"SlotItem"
+											error:&error
+								  predicateFormat:@"id = %@", value];
+	if([array count] == 0) {
+		NSLog(@"SlotItem is invalid.");
+		return nil;
+	}
+	HMKCSlotItemObject *item = array[0];
+	
+	return @(![showsTypes containsObject:item.master_slotItem.type_2]);
+}
+@end
