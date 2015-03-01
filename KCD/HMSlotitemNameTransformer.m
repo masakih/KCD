949769730efbd7e1ba7c9be7f3d3cab545f14ba0
masakih <masakih@users.sourceforge.jp>
2015-03-01 12:24:59 +0900

    装備名を表示するようにした

---
+++ KCD/HMSlotitemNameTransformer.m
@@ -0,0 +1,50 @@
+//
+//  HMSlotitemNameTransformer.m
+//  KCD
+//
+//  Created by Hori,Masaki on 2015/03/01.
+//  Copyright (c) 2015年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMSlotitemNameTransformer.h"
+
+#import "HMServerDataStore.h"
+
+@implementation HMSlotitemNameTransformer
++ (void)load
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		[NSValueTransformer setValueTransformer:[self new] forName:@"HMSlotitemNameTransformer"];
+	});
+}
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
++ (BOOL)allowsReverseTransformation
+{
+	return NO;
+}
+
+- (id)transformedValue:(id)value
+{
+	if(![value isKindOfClass:[NSNumber class]]) return nil;
+	NSInteger slotItemID = [value integerValue];
+	if(slotItemID == -1) return nil;
+	
+	HMServerDataStore *store = [HMServerDataStore oneTimeEditor];
+	
+	NSError *error = nil;
+	NSArray *array = [store objectsWithEntityName:@"SlotItem"
+											error:&error
+								  predicateFormat:@"id = %@", value];
+	if([array count] == 0) {
+		NSLog(@"SlotItem is invalid.");
+		return nil;
+	}
+	
+	return [[array[0] valueForKey:@"name"] copy];
+}
+@end
