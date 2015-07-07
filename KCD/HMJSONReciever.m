42d4c074887fe43b7a1bb674f0e121855eb7f2fe
masakih <masakih@users.sourceforge.jp>
2015-07-07 22:08:24 +0900

    enumerateByteRangesUsingBlock:を使うように戻した

--- KCD/HMJSONReciever.m
+++ KCD/HMJSONReciever.m
@@ -59,12 +59,12 @@
 {
 	NSMutableData *loadedData = [self dataForProtocol:protocol];
 	if(!loadedData) return;
-	
-	[loadedData appendData:data];
-//	[data enumerateByteRangesUsingBlock:^(const void *bytes, NSRange byteRange, BOOL *stop) {
-//		NSData *newData = [NSData dataWithBytes:bytes length:byteRange.length];
-//		[loadedData appendData:newData];
-//	}];
+		
+	NSData *copyedData = [data copy];
+	[copyedData enumerateByteRangesUsingBlock:^(const void *bytes, NSRange byteRange, BOOL *stop) {
+		NSData *newData = [NSData dataWithBytes:bytes length:byteRange.length];
+		[loadedData appendData:newData];
+	}];
 }
 
 - (void)customHTTPProtocol:(CustomHTTPProtocol *)protocol didCompleteWithError:(NSError *)error
