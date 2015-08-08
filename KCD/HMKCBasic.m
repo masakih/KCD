e0df94f6826643c7a1e1db19fd190ba5c0932bb7
masakih <masakih@users.sourceforge.jp>
2015-08-08 11:36:38 +0900

    Basicエンティティを個別クラスにした

---
+++ KCD/HMKCBasic.m
@@ -0,0 +1,55 @@
+//
+//  HMKCBasic.m
+//  KCD
+//
+//  Created by Hori,Masaki on 2015/08/08.
+//  Copyright (c) 2015年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMKCBasic.h"
+
+
+@implementation HMKCBasic
+
+@dynamic active_flag;
+@dynamic comment;
+@dynamic comment_id;
+@dynamic count_deck;
+@dynamic count_kdock;
+@dynamic count_ndock;
+@dynamic experience;
+@dynamic fcoin;
+@dynamic firstflag;
+@dynamic fleetname;
+@dynamic furniture_0;
+@dynamic furniture_1;
+@dynamic furniture_2;
+@dynamic furniture_3;
+@dynamic furniture_4;
+@dynamic furniture_5;
+@dynamic large_Dock;
+@dynamic level;
+@dynamic max_chara;
+@dynamic max_kagu;
+@dynamic max_slotitem;
+@dynamic medals;
+@dynamic member_id;
+@dynamic ms_count;
+@dynamic ms_success;
+@dynamic nickname;
+@dynamic nickname_id;
+@dynamic playtime;
+@dynamic pt_challenged;
+@dynamic pt_challenged_win;
+@dynamic pt_lose;
+@dynamic pt_win;
+@dynamic pvp_0;
+@dynamic pvp_1;
+@dynamic rank;
+@dynamic st_lose;
+@dynamic st_win;
+@dynamic starttime;
+@dynamic tutorial;
+@dynamic tutorial_progress;
+
+@end
