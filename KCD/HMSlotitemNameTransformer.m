0274d0ac14e42abf4d55c858dc1aa6411706739f
masakih <masakih@users.sourceforge.jp>
2015-09-03 00:49:47 +0900

    ソースコードないのマルチバイト文字をplistに追い出した

--- KCD/HMSlotitemNameTransformer.m
+++ KCD/HMSlotitemNameTransformer.m
@@ -13,12 +13,29 @@
 #import "HMKCSlotItemObject+Extensions.h"
 #import "HMKCMasterSlotItemObject.h"
 
+static NSDictionary *slotItemShortName = nil;
+
 @implementation HMSlotitemNameTransformer
 + (void)load
 {
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		[NSValueTransformer setValueTransformer:[self new] forName:@"HMSlotitemNameTransformer"];
+		
+		NSBundle *mainBundle = [NSBundle mainBundle];
+		NSString *path = [mainBundle pathForResource:@"SlotItemShortName" ofType:@"plist"];
+		if(!path) {
+			NSLog(@"Could not find SlotItemShortName.plist");
+			NSBeep();
+			return;
+		}
+		id dict = [NSDictionary dictionaryWithContentsOfFile:path];
+		if(!dict) {
+			NSLog(@"SlotItemShortName.plist is not dictionay.");
+			NSBeep();
+			return;
+		}
+		slotItemShortName = dict;
 	});
 }
 
@@ -50,12 +67,9 @@
 	}
 	
 	HMKCSlotItemObject *slotItem = array[0];
-	
-	NSInteger mstID = [slotItem.master_slotItem.id integerValue];
-	
-	if(mstID == 42) return @"修理";
-	if(mstID == 43) return @"女神";
-	if(mstID == 145) return @"糧食";
+	NSString *mstIDString = [slotItem.master_slotItem.id stringValue];
+	NSString *shortName = slotItemShortName[mstIDString];
+	if(shortName) return shortName;
 	
 	return [slotItem.name copy];
 }
