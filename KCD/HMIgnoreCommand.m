5c38d763b4fdb9dc3983f58fcbd0d01d49578624
masakih <masakih@users.sourceforge.jp>
2015-10-25 00:09:54 +0900

    無視するコマンドを追加

--- KCD/HMIgnoreCommand.m
+++ KCD/HMIgnoreCommand.m
@@ -31,7 +31,7 @@ static NSArray *ignoreCommands = nil;
 						   @"/kcsapi/api_req_furniture/music_list",
 						   @"/kcsapi/api_req_furniture/music_play",
 						   @"/kcsapi/api_req_furniture/set_portbgm",
-						   @"/kcsapi/api_req_kaisou/slotset",
+						   @"/kcsapi/api_req_hensei/lock",
 						   @"/kcsapi/api_req_kaisou/slotset",
 						   @"/kcsapi/api_req_kaisou/slotset_ex",
 						   @"/kcsapi/api_req_kaisou/unsetslot_all",
