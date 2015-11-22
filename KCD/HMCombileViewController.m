ada69cf1538feba0109000c7d269257006b00c71
masakih <masakih@users.sourceforge.jp>
2015-11-22 23:35:17 +0900

    HMFleetViewControllerを使用するように変更

--- KCD/HMCombileViewController.m
+++ KCD/HMCombileViewController.m
@@ -8,13 +8,13 @@
 
 #import "HMCombileViewController.h"
 
-#import "HMVerticalFleetViewController.h"
+#import "HMFleetViewController.h"
 
 
 @interface HMCombileViewController ()
 
-@property (nonatomic, strong) HMVerticalFleetViewController* fleet1;
-@property (nonatomic, strong) HMVerticalFleetViewController* fleet2;
+@property (nonatomic, strong) HMFleetViewController* fleet1;
+@property (nonatomic, strong) HMFleetViewController* fleet2;
 
 @property (nonatomic, weak) IBOutlet NSView *placeholder1;
 @property (nonatomic, weak) IBOutlet NSView *placeholder2;
@@ -25,13 +25,13 @@
 
 - (void)awakeFromNib
 {
-	self.fleet1 = [HMVerticalFleetViewController new];
+	self.fleet1 = [HMFleetViewController viewControlerWithViewType:miniVierticalType];
 	[self.fleet1.view setFrame:[self.placeholder1 frame]];
 	[self.fleet1.view setAutoresizingMask:[self.placeholder1 autoresizingMask]];
 	[[self.placeholder1 superview] replaceSubview:self.placeholder1 with:self.fleet1.view];
 	self.fleet1.fleetNumber = 1;
 	
-	self.fleet2 = [HMVerticalFleetViewController new];
+	self.fleet2 = [HMFleetViewController viewControlerWithViewType:miniVierticalType];
 	[self.fleet2.view setFrame:[self.placeholder2 frame]];
 	[self.fleet2.view setAutoresizingMask:[self.placeholder2 autoresizingMask]];
 	[[self.placeholder2 superview] replaceSubview:self.placeholder2 with:self.fleet2.view];
