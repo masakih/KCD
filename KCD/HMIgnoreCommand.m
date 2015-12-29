d5c84192aa88e668a92049a4e58715b8d9a2e04e
masakih <masakih@users.sourceforge.jp>
2015-12-29 12:46:59 +0900

    無視するAPIを追加

--- KCD/HMIgnoreCommand.m
+++ KCD/HMIgnoreCommand.m
@@ -37,6 +37,7 @@ static NSArray *ignoreCommands = nil;
 						   @"/kcsapi/api_req_furniture/set_portbgm",
 						   @"/kcsapi/api_req_hensei/lock",
 						   @"/kcsapi/api_req_hensei/preset_register",
+						   @"/kcsapi/api_req_kaisou/marriage",
 						   @"/kcsapi/api_req_kaisou/remodeling",
 						   @"/kcsapi/api_req_kaisou/slotset",
 						   @"/kcsapi/api_req_kaisou/slotset_ex",
