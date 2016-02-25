1e35cf623ae8431502a0f058c11bfcd9b6c315ce
masakih <masakih@users.sourceforge.jp>
2016-02-25 20:47:36 +0900

    損傷状態の表示を変更

---
+++ KCD/HMDamageValueTransformer.h
@@ -0,0 +1,13 @@
+//
+//  HMDamageValueTransformer.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2016/02/23.
+//  Copyright © 2016年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMDamageValueTransformer : NSValueTransformer
+
+@end
