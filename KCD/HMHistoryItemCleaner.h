ab3f68e9fb13fd1a28c59350277cabff7d6d4d72
masakih <masakih@users.sourceforge.jp>
2016-06-28 21:19:48 +0900

    履歴アイテムを削除するクラスを追加

---
+++ KCD/HMHistoryItemCleaner.h
@@ -0,0 +1,15 @@
+//
+//  HMHistoryItemCleaner.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2016/06/27.
+//  Copyright © 2016年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMHistoryItemCleaner : NSObject
+
+- (void)cleanOldHistoryItems;
+
+@end
