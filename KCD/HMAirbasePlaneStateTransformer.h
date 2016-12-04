766a1f41493591a2de0aae6e9518c954fffeadf6
masakih <masakih@users.sourceforge.jp>
2016-12-04 22:04:07 +0900

    基地航空隊の状態を文字列に変換するバリュートランスフォーを追加

---
+++ KCD/HMAirbasePlaneStateTransformer.h
@@ -0,0 +1,13 @@
+//
+//  HMAirbasePlaneStateTransformer.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2016/12/04.
+//  Copyright © 2016年 Hori,Masaki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HMAirbasePlaneStateTransformer : NSValueTransformer
+
+@end
