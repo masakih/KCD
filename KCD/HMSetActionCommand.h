318d7ce0bc8f0724a14f777b51bd2d83cf80588a
masakih <masakih@users.sourceforge.jp>
2016-12-04 13:01:40 +0900

    /kcsapi/api_req_air_corps/set_actionに対応

---
+++ KCD/HMSetActionCommand.h
@@ -0,0 +1,13 @@
+//
+//  HMSetActionCommand.h
+//  KCD
+//
+//  Created by Hori,Masaki on 2016/12/04.
+//  Copyright © 2016年 Hori,Masaki. All rights reserved.
+//
+
+#import "HMJSONCommand.h"
+
+@interface HMSetActionCommand : HMJSONCommand
+
+@end
