bd07f15dbb4f9a407df51cb407cccff607d665fd
masakih <masakih@users.sourceforge.jp>
2017-03-11 00:32:15 +0900

    HistoryWindowControllerのtabViewのコンテントを分離した

---
+++ KCD/HistoryTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  HistoryTableViewController.swift
+//  KCD
+//
+//  Created by Hori,Masaki on 2017/03/10.
+//  Copyright © 2017年 Hori,Masaki. All rights reserved.
+//
+
+import Cocoa
+
+fileprivate protocol Markable {
+    var marked: Bool { get set }
+}
+fileprivate protocol HistoryObject {
+    var date: Date { get }
+}
+
+extension DropShipHistory: Markable, HistoryObject {
+    var marked: Bool {
+        get { return mark }
+        set { mark = newValue }
+    }
+}
+extension KenzoHistory: Markable, HistoryObject {
+    var marked: Bool {
+        get { return mark }
+        set { mark = newValue }
+    }
+}
+extension KaihatuHistory: Markable, HistoryObject {
+    var marked: Bool {
+        get { return mark }
+        set { mark = newValue }
+    }
+}
+
+enum MenuItemTag: Int {
+    case delete = 314
+    
+    case addMark = 50000
+}
+
+class HistoryTableViewController: NSViewController {
+    var pickUpPredicateFormat: String { return "date = %@" }
+    var predicateFormat: String { return "" }
+    var entity: Entity? { return nil }
+    
+    @IBOutlet var controller: NSArrayController!
+    @IBOutlet var tableView: NSTableView!
+    
+    @IBAction func delete(_ sender: AnyObject?) {
+        let store = LocalDataStore.oneTimeEditor()
+        guard let controller = controller,
+            let selection = controller.selectedObjects as? [NSManagedObject]
+            else { return }
+        let selectedIndex = controller.selectionIndex
+        selection
+            .forEach { store.delete(store.object(with: $0.objectID)) }
+        if selectedIndex > 1 {
+            controller.setSelectionIndex(selectedIndex - 1)
+        }
+    }
+    @IBAction func addMark(_ sender: AnyObject?) {
+        guard let entity = entity,
+            let clickedRow = tableView?.clickedRow,
+            let items = controller?.arrangedObjects as? [HistoryObject],
+            0..<items.count ~= clickedRow
+            else { return }
+        let clickedObject = items[clickedRow]
+        let predicate = NSPredicate(format: pickUpPredicateFormat,
+                                    argumentArray: [clickedObject.date])
+        let store = LocalDataStore.oneTimeEditor()
+        if let a = try? store.objects(with: entity,
+                                      predicate: predicate),
+            let item = a.first,
+            var history = item as? Markable {
+            history.marked = !history.marked
+        }
+        store.saveActionCore()
+    }
+    
+    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        guard let itemTag = MenuItemTag(rawValue: menuItem.tag)
+            else { return false }
+        switch itemTag {
+        case .delete:
+            return controller.selectionIndex != -1
+        case .addMark:
+            menuItem.isEnabled = false
+            guard let clickedRow = tableView?.clickedRow,
+                let items = controller?.arrangedObjects as? [Markable],
+                0..<items.count ~= clickedRow
+                else { return false }
+            let clickedObject = items[clickedRow]
+            menuItem.isEnabled = true
+            if clickedObject.marked {
+                menuItem.title = NSLocalizedString("Remove mark", comment: "Remove history mark.")
+            } else {
+                menuItem.title = NSLocalizedString("Add mark", comment: "Add history mark.")
+            }
+        }
+        
+        return true
+    }
+}
+extension HistoryTableViewController: NSTableViewDelegate {
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        return tableColumn
+            .flatMap { $0.identifier }
+            .flatMap { tableView.make(withIdentifier: $0, owner: nil) }
+    }
+}
+
+class KaihatsuHistoryTableViewController: HistoryTableViewController {
+    override var predicateFormat: String { return "name contains $value" }
+    override var entity: Entity? { return .kaihatuHistory }
+}
+class KenzoHistoryTableViewController: HistoryTableViewController {
+    override var predicateFormat: String { return "name contains $value" }
+    override var entity: Entity? { return .kenzoHistory }
+}
+class DropShipHistoryTableViewController: HistoryTableViewController {
+    override var predicateFormat: String { return "shipName contains $value" }
+    override var entity: Entity? { return .dropShipHistory }
+}
--- KCD/HistoryWindowController.swift
+++ KCD/HistoryWindowController.swift
@@ -8,32 +8,6 @@
 
 import Cocoa
 
-fileprivate protocol Markable {
-    var marked: Bool { get set }
-}
-extension DropShipHistory: Markable {
-    var marked: Bool {
-        get { return mark }
-        set { mark = newValue }
-    }
-}
-extension KenzoHistory: Markable {
-    var marked: Bool {
-        get { return mark }
-        set { mark = newValue }
-    }
-}
-extension KaihatuHistory: Markable {
-    var marked: Bool {
-        get { return mark }
-        set { mark = newValue }
-    }
-}
-
-fileprivate extension Selector {
-    static let addMark = #selector(HistoryWindowController.addMark(_:))
-}
-
 class HistoryWindowController: NSWindowController {
     fileprivate enum HistoryWindowTabIndex: Int {
         case kaihatuHistory = 0
@@ -41,56 +15,45 @@ class HistoryWindowController: NSWindowController {
         case dropHistory = 2
     }
     
-    fileprivate struct SelectionInfo {
-        let controller: NSArrayController
-        let predicateFormat: String
-        let tableView: NSTableView
-        let entity: Entity
-        let pickUpPredicateFormat: String
-        
-        init(_ owner: HistoryWindowController) {
-            switch owner.swiftSelectedTabIndex {
-            case .kaihatuHistory:
-                controller = owner.kaihatuHistoryController
-                predicateFormat = "name contains $value"
-                tableView = owner.kaihatuHistoryTableView
-                entity = .kaihatuHistory
-                pickUpPredicateFormat = "date = %@ AND name = %@"
-            case .kenzoHistory:
-                controller = owner.kenzoHistoryController
-                predicateFormat = "name contains $value"
-                tableView = owner.kenzoHistoryTableView
-                entity = .kenzoHistory
-                pickUpPredicateFormat = "date = %@ AND name = %@"
-            case .dropHistory:
-                controller = owner.dropHistoryController
-                predicateFormat = "shipName contains $value"
-                tableView = owner.dropHistoryTableView
-                entity = .dropShipHistory
-                pickUpPredicateFormat = "date = %@ AND mapCell = %ld"
+    let manageObjectContext = LocalDataStore.default.managedObjectContext
+    
+    @IBOutlet var tabView: NSTabView!
+    @IBOutlet var kaihatsuTableVC: KaihatsuHistoryTableViewController!
+    @IBOutlet var kenzoTableVC: KenzoHistoryTableViewController!
+    @IBOutlet var dropShipTableVC: DropShipHistoryTableViewController!
+    
+    @IBOutlet var searchField: NSSearchField!
+    
+    private var currentSelection: HistoryTableViewController? {
+        didSet {
+            window?.makeFirstResponder(currentSelection?.tableView)
+            
+            if let controller = currentSelection?.controller,
+                let predicateFormat = currentSelection?.predicateFormat {
+            searchField.bind(NSPredicateBinding,
+                             to: controller,
+                             withKeyPath: NSFilterPredicateBinding,
+                             options: [NSPredicateFormatBindingOption: predicateFormat])
+            } else {
+                searchField.unbind(NSPredicateBinding)
             }
         }
     }
     
-    let manageObjectContext = LocalDataStore.default.managedObjectContext
-    
-    @IBOutlet var kaihatuHistoryController: NSArrayController!
-    @IBOutlet var kenzoHistoryController: NSArrayController!
-    @IBOutlet var dropHistoryController: NSArrayController!
-    
-    @IBOutlet var kaihatuHistoryTableView: NSTableView!
-    @IBOutlet var kenzoHistoryTableView: NSTableView!
-    @IBOutlet var dropHistoryTableView: NSTableView!
-    
-    @IBOutlet var searchField: NSSearchField!
-    
-    var selectedTabIndex: Int = 0 {
+    var selectedTabIndex: Int = -1 {
         didSet {
-            HistoryWindowTabIndex(rawValue: selectedTabIndex)
-                .map { swiftSelectedTabIndex = $0 }
+            guard let tabIndex = HistoryWindowTabIndex(rawValue: selectedTabIndex)
+                else { return }
+            switch tabIndex {
+            case .kaihatuHistory:
+                currentSelection = kaihatsuTableVC
+            case .kenzoHistory:
+                currentSelection = kenzoTableVC
+            case .dropHistory:
+                currentSelection = dropShipTableVC
+            }
         }
     }
-    fileprivate var swiftSelectedTabIndex: HistoryWindowTabIndex = .kaihatuHistory
     override var windowNibName: String! {
         return "HistoryWindowController"
     }
@@ -98,82 +61,15 @@ class HistoryWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         
-        let info = SelectionInfo(self)
+        let vcs: [NSViewController] = [kaihatsuTableVC, kenzoTableVC, dropShipTableVC]
+        zip(vcs, tabView.tabViewItems)
+            .forEach { $0.1.viewController = $0.0 }
         
-        searchField.bind(NSPredicateBinding,
-                         to: info.controller,
-                         withKeyPath: NSFilterPredicateBinding,
-                         options: [NSPredicateFormatBindingOption: info.predicateFormat])
+        selectedTabIndex = 0
     }
     
     @IBAction func delete(_ sender: AnyObject?) {
-        let store = LocalDataStore.oneTimeEditor()
-        SelectionInfo(self)
-            .controller
-            .selectedObjects
-            .flatMap { $0 as? [NSManagedObject] }
-            .flatMap { $0 }
-            .map { $0.objectID }
-            .map { store.object(with: $0) }
-            .forEach { store.delete($0) }
-    }
-    
-    @IBAction func addMark(_ sender: AnyObject?) {
-        let info = SelectionInfo(self)
-        let clickedRow = info.tableView.clickedRow
-        guard let items = info.controller.arrangedObjects as? [Any],
-            0..<items.count ~= clickedRow
-            else { return }
-        let clickedObject = items[clickedRow]
-        
-        let p: NSPredicate? = {
-            switch clickedObject {
-            case let obj as KaihatuHistory:
-                return NSPredicate(format: info.pickUpPredicateFormat,
-                                   argumentArray: [obj.date, obj.name])
-            case let obj as KenzoHistory:
-                return NSPredicate(format: info.pickUpPredicateFormat,
-                                   argumentArray: [obj.date, obj.name])
-            case let obj as DropShipHistory:
-                return NSPredicate(format: info.pickUpPredicateFormat,
-                                   argumentArray: [obj.date, obj.mapCell])
-            default:
-                return nil
-            }
-        }()
-        guard let predicate = p else { return }
-        
-        let store = LocalDataStore.oneTimeEditor()
-        if let a = try? store.objects(with: info.entity,
-                             predicate: predicate),
-            let item = a.first,
-            var history = item as? Markable {
-            history.marked = !history.marked
-        }
-        store.saveActionCore()
-    }
-    
-    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
-        if menuItem.action == .addMark {
-            menuItem.isEnabled = false
-            
-            let info = SelectionInfo(self)
-            let clickedRow = info.tableView.clickedRow
-            guard let items = info.controller.arrangedObjects as? [Any],
-                0..<items.count ~= clickedRow,
-                let clickedObject = items[clickedRow] as? Markable
-                else { return false }
-            
-            menuItem.isEnabled = true
-            if clickedObject.marked {
-                menuItem.title = NSLocalizedString("Remove mark", comment: "Remove history mark.")
-            } else {
-                menuItem.title = NSLocalizedString("Add mark", comment: "Add history mark.")
-            }
-            return true
-        }
-        
-        return false
+        currentSelection?.delete(sender)
     }
 }
 
@@ -216,24 +112,5 @@ extension HistoryWindowController {
     
     @IBAction func selectSearchField(_ sender: AnyObject?) {
         window!.makeFirstResponder(searchField!)
-    }
-}
-
-extension HistoryWindowController: NSTableViewDelegate {
-    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
-        return tableView.make(withIdentifier: tableColumn!.identifier, owner: nil)
-    }
-}
-
-extension HistoryWindowController: NSTabViewDelegate {
-    func tabView(_ tabView: NSTabView, didSelect tabViewItem: NSTabViewItem?) {
-        searchField.unbind(NSPredicateBinding)
-        
-        let info = SelectionInfo(self)
-        
-        searchField.bind(NSPredicateBinding,
-                         to: info.controller,
-                         withKeyPath: NSFilterPredicateBinding,
-                         options: [NSPredicateFormatBindingOption: info.predicateFormat])
     }
 }
