ef6f8ffd48674369c67d852f154794bd1b651bfc
masakih <masakih@users.sourceforge.jp>
2017-03-28 21:38:38 +0900

    ArrayをSetに修正

--- KCD/ArrayExtensions.swift
+++ KCD/ArrayExtensions.swift
@@ -9,18 +9,16 @@
 import Foundation
 
 // powered by http://stackoverflow.com/questions/40579554/how-to-display-unique-elements-of-an-array-using-swift
-// swiftlint:disable syntactic_sugar
 extension Sequence where Iterator.Element: Hashable {
     func unique() -> [Iterator.Element] {
-        var alreadyAdded = Array<Iterator.Element>()
+        var alreadyAdded = Set<Iterator.Element>()
         return filter {
             if alreadyAdded.contains($0) { return false }
-            alreadyAdded.append($0)
+            alreadyAdded.insert($0)
             return true
         }
     }
 }
-// swiftlint:enable syntactic_sugar
 
 extension Array {
     func appended(_ elem: () -> Element) -> Array {
