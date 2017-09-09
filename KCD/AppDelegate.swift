1b0702e699faab4ca453f0cfff3a374bf6427394
masakih <masakih@users.sourceforge.jp>
2017-09-09 20:47:42 +0900

    バージョンを1.9b17に更新

--- KCD/AppDelegate.swift
+++ KCD/AppDelegate.swift
@@ -27,7 +27,7 @@ final class AppDelegate: NSObject {
         return NSApplication.shared().delegate as! AppDelegate
     }
     
-    let appNameForUserAgent: String = "KCD(1.9b16) is not Safari/603.3.8"
+    let appNameForUserAgent: String = "KCD(1.9b17) is not Safari/603.3.8"
     let fleetManager: FleetManager = FleetManager()
     
     let windowManager = WindowManager()
