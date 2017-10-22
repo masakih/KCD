36288547234653d1f048387bf221101d65abd389
masakih <masakih@users.sourceforge.jp>
2017-10-23 00:20:33 +0900

    テストを一つにまとめた

--- KCDTests/NormalBattleTest.swift
+++ KCDTests/NormalBattleTest.swift
@@ -1,5 +1,5 @@
 //
-//  NormalBattleAPIsTest.swift
+//  NormalBattleTest.swift
 //  KCDTests
 //
 //  Created by Hori,Masaki on 2017/10/19.
@@ -12,16 +12,14 @@ import XCTest
 
 import SwiftyJSON
 
-class NormalBattleAPIsTest: XCTestCase {
-
+class NormalBattleTest: XCTestCase {
+    
     var savedShips: [Ship] = []
     var shipsHp: [Int] = []
     var shipEquipments: [NSOrderedSet] = []
     var shipExSlot: [Int] = []
     
-    override func setUp() {
-        
-        super.setUp()
+    func initBattleFleet(_ fleet: Int) {
         
         savedShips = []
         shipsHp = []
@@ -31,10 +29,14 @@ class NormalBattleAPIsTest: XCTestCase {
         // 艦隊を設定
         do {
             let store = ServerDataStore.oneTimeEditor()
-            guard let deck = store.deck(by: 1) else { return XCTFail("Can not get Deck.") }
+            
+            guard let secondFleet = store.deck(by: 2) else { return XCTFail("Can not get Deck.") }
+            (0...5).forEach { secondFleet.setShip(id: $0 + 10, for: $0) }
+            
+            guard let deck = store.deck(by: fleet) else { return XCTFail("Can not get Deck.") }
             (0...5).forEach { deck.setShip(id: $0 + 1, for: $0) }
             
-            store.ships(byDeckId: 1).forEach {
+            store.ships(byDeckId: fleet).forEach {
                 $0.nowhp = $0.maxhp
                 
                 savedShips += [$0]
@@ -56,12 +58,12 @@ class NormalBattleAPIsTest: XCTestCase {
             XCTAssertNotNil(json["api_result"])
             
             let paramValue: [String: String] = [
-                "api_deck_id": "1",
+                "api_deck_id": "\(fleet)",
                 "api_maparea_id": "1",
                 "api_mapinfo_no": "1"
             ]
             let param = Parameter(paramValue)
-            XCTAssertEqual(param["api_deck_id"].string, "1")
+            XCTAssertEqual(param["api_deck_id"].string, "\(fleet)")
             
             let api = APIResponse(api: MapAPI.start.rawValue, parameter: param, json: json)
             XCTAssertEqual(api.json, json)
@@ -77,10 +79,11 @@ class NormalBattleAPIsTest: XCTestCase {
             let store = TemporaryDataStore.default
             let battle = store.battle()
             XCTAssertNotNil(battle)
+            XCTAssertEqual(battle?.deckId, fleet)
         }
     }
     
-    override func tearDown() {
+    func clear(_ fleet: Int) {
         
         do {
             ResetSortie().reset()
@@ -95,19 +98,18 @@ class NormalBattleAPIsTest: XCTestCase {
         do {
             let store = ServerDataStore.oneTimeEditor()
             
-            let ships = store.ships(byDeckId: 1)
+            let ships = store.ships(byDeckId: fleet)
             
             zip(ships, shipsHp).forEach { $0.nowhp = $1 }
             zip(ships, shipEquipments).forEach { $0.equippedItem = $1 }
             zip(ships, shipExSlot).forEach { $0.slot_ex = $1 }
             
-            guard let deck = store.deck(by: 1) else { return XCTFail("Can not get Deck.") }
+            guard let deck = store.deck(by: fleet) else { return XCTFail("Can not get Deck.") }
             savedShips.enumerated().forEach { deck.setShip(id: $0.element.id, for: $0.offset) }
         }
-        super.tearDown()
     }
-
-    func testNormalBattle() {
+    
+    func normalBattle(_ fleet: Int) {
         
         // 戦闘（昼戦）
         do {
@@ -192,7 +194,7 @@ class NormalBattleAPIsTest: XCTestCase {
         // HPチェック
         do {
             let store = ServerDataStore.oneTimeEditor()
-            let ships = store.ships(byDeckId: 1)
+            let ships = store.ships(byDeckId: fleet)
             
             XCTAssertEqual(ships.count, 6)
             
@@ -205,7 +207,7 @@ class NormalBattleAPIsTest: XCTestCase {
         }
     }
     
-    func testDamageControl() {
+    func damageControl(_ fleet: Int) {
         
         // ダメコンを設定
         do {
@@ -267,7 +269,7 @@ class NormalBattleAPIsTest: XCTestCase {
         // HPチェック
         do {
             let store = ServerDataStore.oneTimeEditor()
-            let ships = store.ships(byDeckId: 1)
+            let ships = store.ships(byDeckId: fleet)
             
             XCTAssertEqual(ships.count, 6)
             
@@ -278,5 +280,38 @@ class NormalBattleAPIsTest: XCTestCase {
             XCTAssertEqual(ships[4].nowhp, shipsHp[4])
             XCTAssertEqual(ships[5].nowhp, Int(Double(shipsHp[5]) * 0.2))
         }
+    }
+    
+    func testFirstFleet() {
+        
+        initBattleFleet(1)
+        normalBattle(1)
+        initBattleFleet(1)
+        damageControl(1)
+        clear(1)
+    }
+    func testSecondFleet() {
+        
+        initBattleFleet(2)
+        normalBattle(2)
+        initBattleFleet(2)
+        damageControl(2)
+        clear(2)
+    }
+    func testThiredFleet() {
+        
+        initBattleFleet(3)
+        normalBattle(3)
+        initBattleFleet(3)
+        damageControl(3)
+        clear(3)
+    }
+    func testFourthFleet() {
+        
+        initBattleFleet(4)
+        normalBattle(4)
+        initBattleFleet(4)
+        damageControl(4)
+        clear(4)
     }
 }
