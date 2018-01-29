0f3dffcdecc249b2576b47ca257d070de37df585
masakih <masakih@users.sourceforge.jp>
2018-01-29 23:00:00 +0900

    NotificationObserverクラスを追加

---
+++ KCD/NotificationObserver.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationObserver.swift
+//  KCD
+//
+//  Created by Hori,Masaki on 2018/01/28.
+//  Copyright © 2018年 Hori,Masaki. All rights reserved.
+//
+
+import Foundation
+
+/// 開放時にremoveObserver(_:)を実行してくれるクラス
+class NotificationObserver {
+    
+    private var tokens: [NSObjectProtocol] = []
+    
+    func addObserver(forName name: Notification.Name, object: Any?, queue: OperationQueue? = nil, using block: @escaping (Notification) -> Void) {
+        
+        tokens += [NotificationCenter.default.addObserver(forName: name, object: object, queue: queue, using: block)]
+    }
+    
+    deinit {
+        tokens.forEach(NotificationCenter.default.removeObserver)
+    }
+}
